e55a15a55aefd339f37f70b7533252570a686991
Pranjal Srivastava <75536136+iampsrv@users.noreply.github.com>
2023-03-25 14:21:51 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ from flask import Flask
 app = Flask(__name__)
 @app.route('/')
 def index():
-    return 'Web Application with Python Flask!'
+    return 'Web Application with Python Flask! xyz'
 
 app.run(host='0.0.0.0', port=5000, debug=True)
