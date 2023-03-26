c8b1943ae675ea6e23405c36a8eb199124431943
Pranjal Srivastava <75536136+iampsrv@users.noreply.github.com>
2023-03-26 13:30:40 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,17 +1,17 @@
 import pytest
-from factorial import factorial
+from my_factorial import my_factorial
 
 def test_factorial_1():
-    assert factorial(0) == 1
+    assert my_factorial(0) == 1
 
 def test_factorial_2():
-    assert factorial(1) == 1
+    assert my_factorial(1) == 1
 
 def test_factorial_3():
-    assert factorial(2) == 2
+    assert my_factorial(2) == 2
 
 def test_factorial_4():
-    assert factorial(5) == 120
+    assert my_factorial(5) == 120
 
 def test_factorial_5():
-    assert factorial(10) == 3628800
+    assert my_factorial(10) == 3628800
